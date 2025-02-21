17a5b2604433c0496d71a913615f37aa07b9e0f8
Daniel Da Silva Carvalho <danielda@student.42sp.org.br>
2025-02-21 17:48:15 -0300

    ok

--- tokens/token_utils.c
+++ tokens/token_utils.c
@@ -6,12 +6,29 @@
 /*   By: danielda <danielda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 22:18:17 by danielda          #+#    #+#             */
-/*   Updated: 2025/02/21 17:03:22 by danielda         ###   ########.fr       */
+/*   Updated: 2025/02/21 17:47:55 by danielda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void set_operator_type(t_token **tokens) //Ajusta o tipo de operadores (| vira PIPE, < vira REDIR_IN, etc.).
 {
+	t_token	*current;
+
+	current = *tokens;
+	while (current != NULL)
+	{
+		if (strcmp(current->value, "|") == 0)
+			current->type = PIPE;
+		else if (strcmp(current->value, "<") == 0)
+			current->type = REDIR_IN;
+		else if (strcmp(current->value, ">") == 0)
+			current->type = REDIR_OUT;
+		else if (strcmp(current->value, "<<") == 0)
+			current->type = HEREDOC;
+		else if (strcmp(current->value, ">>") == 0)
+			current->type = APPEND;
+		current = current->next;
+	}
 }
 void set_redir_type(t_token *tokens, char type) //Define HEREDOC, APPEND, REDIR_IN ou REDIR_OUT.
 {
