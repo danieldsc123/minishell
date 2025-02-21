2162ead551480a718782512ca16362819ca161f8
Daniel Da Silva Carvalho <danielda@student.42sp.org.br>
2025-02-21 15:20:15 -0300

    minishell.h sem teste

---
+++ minishell.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danielda <danielda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 15:16:26 by danielda          #+#    #+#             */
+/*   Updated: 2025/02/21 15:19:30 by danielda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <limits.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <signal.h>
+# include <termios.h>
+# include <string.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <dirent.h>
+
+struct s_token
+{
+};
