2e2c2a85d12e4f5ce91bd13217e70617144c6fb2
Daniel Da Silva Carvalho <danielda@student.42sp.org.br>
2025-02-21 16:39:10 -0300

    teste tonkens.c

--- tokens/tokens.c
+++ tokens/tokens.c
@@ -6,7 +6,39 @@
 /*   By: danielda <danielda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 21:08:18 by danielda          #+#    #+#             */
-/*   Updated: 2025/02/20 21:08:19 by danielda         ###   ########.fr       */
+/*   Updated: 2025/02/21 16:37:49 by danielda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+#include <string.h> // Para strndup
+#include <stdlib.h> // Para malloc e free
+
+t_token	*allocate_token(t_token **tokens, char *input, int start, int end)
+{
+	t_token	*new_token;
+	t_token	*current;
+
+	new_token = malloc(sizeof(t_token));
+	if (!new_token)
+		return (NULL);
+	new_token->value = ft_strndup(input + start, end - start);
+	if (!new_token->value)
+	{
+		free(new_token);
+		return (NULL);
+	}
+	new_token->next = NULL;
+	if (*tokens == NULL)
+	{
+		*tokens = new_token;
+	}
+	else
+	{
+		current = *tokens;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new_token;
+	}
+	return (new_token);
+}
