88b3dca4fa3f9229e6745b896f2a2681a7fdebcd
Daniel Da Silva Carvalho <danielda@student.42sp.org.br>
2025-02-21 15:06:01 -0300

    lexer.c sem teste

--- tokens/lexer.c
+++ tokens/lexer.c
@@ -6,7 +6,32 @@
 /*   By: danielda <danielda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 22:17:45 by danielda          #+#    #+#             */
-/*   Updated: 2025/02/20 22:17:46 by danielda         ###   ########.fr       */
+/*   Updated: 2025/02/21 14:51:28 by danielda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "tokens.h"
+
+t_token	*lexer(char *input)
+{
+	t_token	*token;
+	t_token	*head;
+	t_token	*prev;
+	int		i;
+
+	i = 0;
+	head = NULL;
+	prev = NULL;
+	while (input[i])
+	{
+		token = new_token(input, &i);
+		if (token == NULL)
+			return (NULL);
+		if (prev)
+			prev->next = token;
+		else
+			head = token;
+		prev = token;
+	}
+	return (head);
+}
