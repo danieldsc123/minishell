7d9afb0102736028c76dbc0555d29def0ea20501
Daniel Da Silva Carvalho <danielda@student.42sp.org.br>
2025-02-24 23:31:06 -0300

    hvfd

--- tokens/lexer.c
+++ tokens/lexer.c
@@ -6,11 +6,11 @@
 /*   By: danielda <danielda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 22:17:45 by danielda          #+#    #+#             */
-/*   Updated: 2025/02/24 23:17:32 by danielda         ###   ########.fr       */
+/*   Updated: 2025/02/24 23:22:48 by danielda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
+#include "minishell.h"
 
 t_token	*lexer(char *input)
 {
